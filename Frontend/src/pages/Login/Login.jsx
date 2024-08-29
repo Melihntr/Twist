5d2094ddd7534f104a3aca6417ed3a297e3b51d7
Ayhan Taha Pastutmaz <113910977+ayhan219@users.noreply.github.com>
2024-08-29 12:46:09 +0300

    Update Login.jsx

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const Login = () => {
+const Login = ({handleUserLogin}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
